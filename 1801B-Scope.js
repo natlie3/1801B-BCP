c54f134a1e91754467f1dd9bdfdf304aed4b3987
Eliot Szwajkowski <eliotpszw@gmail.com>
2018-02-06 18:43:20 -0500

    Initial Repo.

---
+++ 1801B-Scope.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk');
+
+console.log(chalk.red('---------------------------------'));
+
+console.log('hello');
+
+console.log('goodbye!');
+
+console.log('goodbye again!');
---
+++ README.md
@@ -0,0 +1,3 @@
+## 1801B-BCP Prep Repository
+
+**WIP**
